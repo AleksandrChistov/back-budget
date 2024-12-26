c034a355df21c37bf3240ab906fc662ccfd96ce1
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2024-12-26 11:57:49 +0700

    add properties for jpa

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO ACCESSES (full_name, email, password, role)
+VALUES ('Иванов Иван Иванович', 'admin@yandex.ru', '{noop}admin', 'ADMIN'),
+       ('Петров Петр Петрович', 'manager@yandex.ru', '{noop}manager', 'MANAGER'),
+       ('Маринова Марина Мариновна', 'analyst@yandex.ru', '{noop}analyst', 'ANALYST');
