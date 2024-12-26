611d5aae443c2d9af98646b09ffc8ca5277c8db9
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2024-12-26 18:15:54 +0700

    edit Account entity

--- src/main/java/ru/aleksandrchistov/budget/account/Account.java
+++ src/main/java/ru/aleksandrchistov/budget/account/Account.java
@@ -1,6 +1,8 @@
 package ru.aleksandrchistov.budget.account;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -8,6 +10,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import ru.aleksandrchistov.budget.common.model.BaseEntity;
 
 import java.math.BigDecimal;
@@ -18,6 +22,7 @@ import java.math.BigDecimal;
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Account extends BaseEntity {
+
     @Column(name = "num", unique = true, nullable = false)
     @NotNull
     private Integer num;
@@ -35,5 +40,36 @@ public class Account extends BaseEntity {
     @NotBlank
     @Size(max = 256)
     private String name;
+
+    @Column(name = "department_id")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OnDelete(action = OnDeleteAction.SET_DEFAULT)
+    @Min(1)
+    private Integer departmentId;
+
+    public Account(Account acc) {
+        this(acc.id, acc.num, acc.type, acc.balance, acc.name, acc.departmentId);
+    }
+
+    public Account(Integer id, Integer num, AccountType type, BigDecimal balance, String name, Integer departmentId) {
+        super(id);
+        setNum(num);
+        setType(type);
+        setBalance(balance);
+        setName(name);
+        setDepartmentId(departmentId);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", num=" + num +
+                ", type=" + type +
+                ", balance=" + balance +
+                ", departmentId=" + departmentId +
+                ", name='" + name + '\'' +
+                '}';
+    }
 
 }
