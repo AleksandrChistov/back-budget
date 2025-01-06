a9722970bf351e7a026837d1c310e2166df29b7a
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-01-06 11:49:13 +0700

    add calculation of expenses in ReportController

--- src/main/java/ru/aleksandrchistov/budget/pages/report/ReportController.java
+++ src/main/java/ru/aleksandrchistov/budget/pages/report/ReportController.java
@@ -8,17 +8,13 @@ import org.springframework.web.bind.annotation.*;
 import ru.aleksandrchistov.budget.pages.budget.BudgetPlanRepository;
 import ru.aleksandrchistov.budget.pages.budget.model.BudgetMonth;
 import ru.aleksandrchistov.budget.pages.report.dto.ReportDto;
-import ru.aleksandrchistov.budget.pages.report.dto.ReportMonthDto;
-import ru.aleksandrchistov.budget.pages.report.dto.TotalMonthDto;
 import ru.aleksandrchistov.budget.pages.transaction.Transaction;
 import ru.aleksandrchistov.budget.pages.transaction.TransactionRepository;
 import ru.aleksandrchistov.budget.pages.transaction.TransactionType;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 
 import static org.slf4j.LoggerFactory.getLogger;
 import static ru.aleksandrchistov.budget.pages.report.ReportUtility.*;
@@ -41,157 +37,29 @@ public class ReportController {
     public ReportDto getAll(@RequestParam TransactionType type, @Nullable @RequestParam Integer departmentId, @Nullable @RequestParam Integer budgetId) {
         log.info("getAll {}, {}, {}", type, departmentId, budgetId);
         List<Transaction> transactions;
+
         if (departmentId != null) {
             transactions = transactionRepository.getAllByDepartmentId(departmentId);
         } else {
             transactions = transactionRepository.findAll();
         }
 
-        // Транзакции (actual) по статье бюджета по месяцам
         Map<Integer, BigDecimal[]> transactionMonths = getTransactionMonths(transactions);
 
         List<BudgetMonth> planMoths = planRepository.getAllByBudgetId(budgetId);
-        // Планы (plan) по статье бюджета по месяцам
         Map<Integer, BigDecimal[]> planDtos = getPlanMonths(planMoths);
 
         if (type == TransactionType.INCOME) {
             String[] titles = new String[]{"Операционная прибыль", "EBITDA", "Прибыль до налогов", "Чистая прибыль"};
             BigDecimal[][] plans = calcProfits(planDtos);
             BigDecimal[][] actuals = calcProfits(transactionMonths);
-
-            TotalMonthDto operProfitTotal = getTotalDto(titles[0], plans[0], actuals[0]);
-            TotalMonthDto ebitdaTotal = getTotalDto(titles[1], plans[1], actuals[1]);
-            TotalMonthDto finProfitTotal = getTotalDto(titles[2], plans[2], actuals[2]);
-            TotalMonthDto netProfitTotal = getTotalDto(titles[3], plans[3], actuals[3]);
-
-            ReportMonthDto operProfitReport = new ReportMonthDto(titles[0], plans[0], actuals[0]);
-            ReportMonthDto ebitdaReport = new ReportMonthDto(titles[1], plans[1], actuals[1]);
-            ReportMonthDto finProfitReport = new ReportMonthDto(titles[2], plans[2], actuals[2]);
-            ReportMonthDto netProfitReport = new ReportMonthDto(titles[3], plans[3], actuals[3]);
-
-            TotalMonthDto[] totals = new TotalMonthDto[]{operProfitTotal, ebitdaTotal, finProfitTotal, netProfitTotal};
-            ReportMonthDto[] reports = new ReportMonthDto[]{operProfitReport, ebitdaReport, finProfitReport, netProfitReport};
-
-            return new ReportDto(totals, reports);
+            return getReportDto(titles, plans, actuals);
         }
 
-        // TODO: implement calculations for expenses reports
-        // Все EXPENSE не вычитаем, а складываем?
         String[] titles = new String[]{"Операционные расходы", "Оплата труда", "Налоги", "Проценты к уплате"};
-        BigDecimal[][] plans = calcProfits(planDtos);
-        BigDecimal[][] actuals = calcProfits(transactionMonths);
-
-        TotalMonthDto operProfitTotal = getTotalDto(titles[0], plans[0], actuals[0]);
-        TotalMonthDto ebitdaTotal = getTotalDto(titles[1], plans[1], actuals[1]);
-        TotalMonthDto finProfitTotal = getTotalDto(titles[2], plans[2], actuals[2]);
-        TotalMonthDto netProfitTotal = getTotalDto(titles[3], plans[3], actuals[3]);
-
-        ReportMonthDto operProfitReport = new ReportMonthDto(titles[0], plans[0], actuals[0]);
-        ReportMonthDto ebitdaReport = new ReportMonthDto(titles[1], plans[1], actuals[1]);
-        ReportMonthDto finProfitReport = new ReportMonthDto(titles[2], plans[2], actuals[2]);
-        ReportMonthDto netProfitReport = new ReportMonthDto(titles[3], plans[3], actuals[3]);
-
-        TotalMonthDto[] totals = new TotalMonthDto[]{operProfitTotal, ebitdaTotal, finProfitTotal, netProfitTotal};
-        ReportMonthDto[] reports = new ReportMonthDto[]{operProfitReport, ebitdaReport, finProfitReport, netProfitReport};
-
-        return new ReportDto(totals, reports);
-
-        /*
-        * Если type == TransactionTypes.EXPENSE, тогда формируем четыре объекта
-        *   Операционные расходы
-Операционные расходы — это повседневные затраты компании для ведения бизнеса, производства продуктов и услуг.
-заработная плата сотрудников; аренда офисов; расходы на ремонт или техническое обслуживание имущества;
-охрана и благоустройство территории; коммунальные услуги; обучение сотрудников; лицензии на ПО или патенты.
-Операционные затраты противопоставляются прямым затратам — расходам компании на непосредственное создание товаров и услуг.
-BudgetType.EXPENSE (27-39)
-        *   Оплата труда
-BudgetType.EXPENSE (34-36)
-        *   Налоги (на прибыль, сраховые взносы)
-BudgetType.EXPENSES (26 + 35-36)
-        *   Проценты к уплате
-BudgetType.CAPITAL (61-63 и 67-69)
-        *
-        * */
-
-//        List<BudgetItemDto> itemDtos = getItemDtos(items, planDtos);
-//
-//        return budgetDto(itemDtos, type);
-    }
-
-    private BigDecimal[][] calcProfits(Map<Integer, BigDecimal[]> months) {
-        BigDecimal[] operProfit = calcProfit(
-                months,
-                transaction -> (transaction.getKey() > 0 && transaction.getKey() < 21),
-                transaction -> (transaction.getKey() > 26 && transaction.getKey() < 40)
-        );
-
-        BigDecimal[] ebitda = calcProfit(
-                months,
-                transaction ->
-                        (transaction.getKey() > 0 && transaction.getKey() < 26) ||
-                                (transaction.getKey() > 48 && transaction.getKey() < 61) ||
-                                (transaction.getKey() > 63 && transaction.getKey() < 67),
-                transaction ->
-                        (transaction.getKey() > 26 && transaction.getKey() < 32) ||
-                                (transaction.getKey() > 32 && transaction.getKey() < 40) ||
-                                (transaction.getKey() > 39 && transaction.getKey() < 49) ||
-                                (transaction.getKey() > 66 && transaction.getKey() < 70)
-        );
-
-        BigDecimal[] finProfit = calcProfit(
-                months,
-                transaction ->
-                        (transaction.getKey() > 0 && transaction.getKey() < 26) ||
-                                (transaction.getKey() > 48 && transaction.getKey() < 61) ||
-                                (transaction.getKey() > 63 && transaction.getKey() < 67),
-                transaction ->
-                        (transaction.getKey() > 26 && transaction.getKey() < 40) ||
-                                (transaction.getKey() > 39 && transaction.getKey() < 49) ||
-                                (transaction.getKey() > 60 && transaction.getKey() < 64) ||
-                                (transaction.getKey() > 66 && transaction.getKey() < 70)
-        );
-
-        BigDecimal[] netProfit = calcProfit(
-                months,
-                transaction ->
-                        (transaction.getKey() > 0 && transaction.getKey() < 26) ||
-                                (transaction.getKey() > 48 && transaction.getKey() < 61) ||
-                                (transaction.getKey() > 63 && transaction.getKey() < 67),
-                transaction ->
-                        (transaction.getKey() > 25 && transaction.getKey() < 40) ||
-                                (transaction.getKey() > 39 && transaction.getKey() < 49) ||
-                                (transaction.getKey() > 60 && transaction.getKey() < 64) ||
-                                (transaction.getKey() > 66 && transaction.getKey() < 70)
-        );
-
-        return new BigDecimal[][] {operProfit, ebitda, finProfit, netProfit};
-    }
-
-    private BigDecimal[] calcProfit(
-            Map<Integer, BigDecimal[]> items,
-            Predicate<Map.Entry<Integer, BigDecimal[]>> filterProfit,
-            Predicate<Map.Entry<Integer, BigDecimal[]>> filterExpenses
-    ) {
-        BigDecimal[] profits = new BigDecimal[12];
-        Arrays.fill(profits, BigDecimal.ZERO);
-
-        items.entrySet().stream().filter(filterProfit)
-                .forEach(transaction -> {
-                    for (int i = 0; i < profits.length; i++) {
-                        BigDecimal profit = transaction.getValue()[i] != null ? transaction.getValue()[i] : BigDecimal.ZERO;
-                        profits[i] = profits[i].add(profit);
-                    }
-                });
-
-        items.entrySet().stream().filter(filterExpenses)
-                .forEach(transaction -> {
-                    for (int i = 0; i < profits.length; i++) {
-                        BigDecimal expense = transaction.getValue()[i] != null ? transaction.getValue()[i] : BigDecimal.ZERO;
-                        profits[i] = profits[i].subtract(expense);
-                    }
-                });
-
-        return profits;
+        BigDecimal[][] plans = calcExpenses(planDtos);
+        BigDecimal[][] actuals = calcExpenses(transactionMonths);
+        return getReportDto(titles, plans, actuals);
     }
 
 }
--- src/main/java/ru/aleksandrchistov/budget/pages/report/ReportUtility.java
+++ src/main/java/ru/aleksandrchistov/budget/pages/report/ReportUtility.java
@@ -2,6 +2,8 @@ package ru.aleksandrchistov.budget.pages.report;
 
 import lombok.experimental.UtilityClass;
 import ru.aleksandrchistov.budget.pages.budget.model.BudgetMonth;
+import ru.aleksandrchistov.budget.pages.report.dto.ReportDto;
+import ru.aleksandrchistov.budget.pages.report.dto.ReportMonthDto;
 import ru.aleksandrchistov.budget.pages.report.dto.TotalMonthDto;
 import ru.aleksandrchistov.budget.pages.transaction.Transaction;
 
@@ -10,6 +12,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 @UtilityClass
 public class ReportUtility {
@@ -48,10 +51,148 @@ public class ReportUtility {
         return planMonths;
     }
 
-    public static TotalMonthDto getTotalDto(String title, BigDecimal[] plans, BigDecimal[] actuals) {
+    public static ReportDto getReportDto(String[] titles, BigDecimal[][] plans, BigDecimal[][] actuals) {
+        TotalMonthDto total1 = getTotalDto(titles[0], plans[0], actuals[0]);
+        TotalMonthDto total2 = getTotalDto(titles[1], plans[1], actuals[1]);
+        TotalMonthDto total3 = getTotalDto(titles[2], plans[2], actuals[2]);
+        TotalMonthDto total4 = getTotalDto(titles[3], plans[3], actuals[3]);
+
+        ReportMonthDto report1 = new ReportMonthDto(titles[0], plans[0], actuals[0]);
+        ReportMonthDto report2 = new ReportMonthDto(titles[1], plans[1], actuals[1]);
+        ReportMonthDto report3 = new ReportMonthDto(titles[2], plans[2], actuals[2]);
+        ReportMonthDto report4 = new ReportMonthDto(titles[3], plans[3], actuals[3]);
+
+        TotalMonthDto[] totals = new TotalMonthDto[]{total1, total2, total3, total4};
+        ReportMonthDto[] reports = new ReportMonthDto[]{report1, report2, report3, report4};
+
+        return new ReportDto(totals, reports);
+    }
+
+    private static TotalMonthDto getTotalDto(String title, BigDecimal[] plans, BigDecimal[] actuals) {
         BigDecimal plan = Arrays.stream(plans).reduce(BigDecimal.ZERO, BigDecimal::add);
         BigDecimal actual = Arrays.stream(actuals).reduce(BigDecimal.ZERO, BigDecimal::add);
         return new TotalMonthDto(title, plan, actual);
+    }
+
+    public static BigDecimal[][] calcExpenses(Map<Integer, BigDecimal[]> months) {
+        BigDecimal[] operExpenses = calcExpense(
+                months,
+                transaction ->
+                        (transaction.getKey() > 26 && transaction.getKey() < 35) ||
+                        (transaction.getKey() > 36 && transaction.getKey() < 40)
+        );
+
+        BigDecimal[] salaries = calcExpense(
+                months,
+                transaction ->
+                        (transaction.getKey() > 33 && transaction.getKey() < 37)
+        );
+
+        BigDecimal[] taxes = calcExpense(
+                months,
+                transaction ->
+                        (transaction.getKey() == 26) || (transaction.getKey() > 34 && transaction.getKey() < 37)
+        );
+
+        BigDecimal[] percents = calcExpense(
+                months,
+                transaction ->
+                        (transaction.getKey() > 60 && transaction.getKey() < 64) ||
+                                (transaction.getKey() > 66 && transaction.getKey() < 70)
+        );
+
+        return new BigDecimal[][] {operExpenses, salaries, taxes, percents};
+    }
+
+    private static BigDecimal[] calcExpense(Map<Integer, BigDecimal[]> items, Predicate<Map.Entry<Integer, BigDecimal[]>> filterExpenses) {
+        BigDecimal[] expenses = new BigDecimal[12];
+        Arrays.fill(expenses, BigDecimal.ZERO);
+
+        items.entrySet().stream().filter(filterExpenses)
+                .forEach(transaction -> {
+                    for (int i = 0; i < expenses.length; i++) {
+                        BigDecimal expense = transaction.getValue()[i] != null ? transaction.getValue()[i] : BigDecimal.ZERO;
+                        expenses[i] = expenses[i].add(expense);
+                    }
+                });
+
+        return expenses;
+    }
+
+    public static BigDecimal[][] calcProfits(Map<Integer, BigDecimal[]> months) {
+        BigDecimal[] operProfit = calcProfit(
+                months,
+                transaction -> (transaction.getKey() > 0 && transaction.getKey() < 21),
+                transaction -> (transaction.getKey() > 26 && transaction.getKey() < 40)
+        );
+
+        BigDecimal[] ebitda = calcProfit(
+                months,
+                transaction ->
+                        (transaction.getKey() > 0 && transaction.getKey() < 26) ||
+                                (transaction.getKey() > 48 && transaction.getKey() < 61) ||
+                                (transaction.getKey() > 63 && transaction.getKey() < 67),
+                transaction ->
+                        (transaction.getKey() > 26 && transaction.getKey() < 32) ||
+                                (transaction.getKey() > 32 && transaction.getKey() < 40) ||
+                                (transaction.getKey() > 39 && transaction.getKey() < 49) ||
+                                (transaction.getKey() > 66 && transaction.getKey() < 70)
+        );
+
+        BigDecimal[] finProfit = calcProfit(
+                months,
+                transaction ->
+                        (transaction.getKey() > 0 && transaction.getKey() < 26) ||
+                                (transaction.getKey() > 48 && transaction.getKey() < 61) ||
+                                (transaction.getKey() > 63 && transaction.getKey() < 67),
+                transaction ->
+                        (transaction.getKey() > 26 && transaction.getKey() < 40) ||
+                                (transaction.getKey() > 39 && transaction.getKey() < 49) ||
+                                (transaction.getKey() > 60 && transaction.getKey() < 64) ||
+                                (transaction.getKey() > 66 && transaction.getKey() < 70)
+        );
+
+        BigDecimal[] netProfit = calcProfit(
+                months,
+                transaction ->
+                        (transaction.getKey() > 0 && transaction.getKey() < 26) ||
+                                (transaction.getKey() > 48 && transaction.getKey() < 61) ||
+                                (transaction.getKey() > 63 && transaction.getKey() < 67),
+                transaction ->
+                        (transaction.getKey() > 25 && transaction.getKey() < 40) ||
+                                (transaction.getKey() > 39 && transaction.getKey() < 49) ||
+                                (transaction.getKey() > 60 && transaction.getKey() < 64) ||
+                                (transaction.getKey() > 66 && transaction.getKey() < 70)
+        );
+
+        return new BigDecimal[][] {operProfit, ebitda, finProfit, netProfit};
+    }
+
+    private static BigDecimal[] calcProfit(
+            Map<Integer, BigDecimal[]> items,
+            Predicate<Map.Entry<Integer, BigDecimal[]>> filterProfit,
+            Predicate<Map.Entry<Integer, BigDecimal[]>> filterExpenses
+    ) {
+        BigDecimal[] profits = new BigDecimal[12];
+        Arrays.fill(profits, BigDecimal.ZERO);
+
+        items.entrySet().stream().filter(filterProfit)
+                .forEach(transaction -> {
+                    for (int i = 0; i < profits.length; i++) {
+                        BigDecimal profit = transaction.getValue()[i] != null ? transaction.getValue()[i] : BigDecimal.ZERO;
+                        profits[i] = profits[i].add(profit);
+                    }
+                });
+
+        items.entrySet().stream().filter(filterExpenses)
+                .forEach(transaction -> {
+                    for (int i = 0; i < profits.length; i++) {
+                        BigDecimal expense = transaction.getValue()[i] != null ? transaction.getValue()[i] : BigDecimal.ZERO;
+                        profits[i] = profits[i].subtract(expense);
+                    }
+                });
+
+        return profits;
     }
 
 }
